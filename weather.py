2ec78cb49c0d6b7d6e8c0508656b5a8a0ff47104
guilherme <gsfl3101@gmail.com>
2022-09-13 22:38:01 +0200

    Added code to make api call to get weather data

--- weather.py
+++ weather.py
@@ -1,1 +1,18 @@
-API_KEY = "e311671859ff626e1d26c89ec3641920"
+import requests
+
+# API Key for test purposes. Already deactivated
+API_KEY = "85a3afdc696d74fd74011926715a892b"
+BASE_URL = "https://api.openweathermap.org/data/2.5/weather"
+
+city = input("Enter a city name: ")
+
+request_url = f"{BASE_URL}?appid={API_KEY}&q={city}"
+print(request_url)
+response = requests.get(request_url)
+
+if response.status_code == 200:
+    data = response.json()
+    print(data)
+else:
+    print(response.json())
+    print("An error occurred.")
